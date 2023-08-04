a1b86359105918b05ecc8d6e68453d0fd3501b3d
kunal166 <84465191+kunal166@users.noreply.github.com>
2023-08-04 21:24:37 +0530

    Create Style.css

---
+++ Style.css
@@ -0,0 +1,23 @@
+body {
+  background-color: #f0f0f0;
+}
+
+div {
+  background-color: #e5e5e5;
+  color: #333333;
+  padding: 20px;
+}
+
+h1, h2 {
+  font-family: 'Your-Font-Name', sans-serif;
+}
+
+section {
+  border: 1px solid #999999;
+  border-radius: 10px;
+  padding: 10px;
+}
+
+.skills li:hover {
+  color: #ff0000;
+}
